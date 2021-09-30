1d9a917420d3c10c2f9e2ea5f35e162eec0fa72c
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2021-09-30 09:04:45 -0300

    conversor de fahrenheit para celsius

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-function readData() {
+//Conversão de Celsius para Fahrenheit
+function readDataCelsius() {
     var celsius = document.querySelector('[data-js="celsius"]').value;
-    document
-        .querySelector('[data-js="result"]').innerHTML = convert(celsius);
-}
-
-function clearFields(){
-    document.querySelector('[data-js="celsius"]').value = "";
-    document
-        .querySelector('[data-js="result"]').innerHTML = "";
-}
-
-function convert(c) {
-    var fahrenheit = (9 * c + 160) / 5;
+    var fahrenheit = (9 * celsius + 160) / 5;
     var showResult = `Resultado: ${fahrenheit.toFixed(0)}ºF`;
 
-    return (
-        showResult
-    );
+    document
+        .querySelector('[data-js="resultCelsius"]').innerHTML = showResult;
+}
+
+function clearFieldsCelsius(){
+    document.querySelector('[data-js="celsius"]').value = "";
+    document
+        .querySelector('[data-js="resultCelsius"]').innerHTML = "";
+}
+
+
+//Conversão de Fahrenheit
+function readDataFahrenheit() {
+    var fahrenheit = document.querySelector('[data-js="fahrenheit"]').value;
+    var celsius = (fahrenheit - 32) * (5 / 9);
+    var showResultInFahrenheit = `Resultado: ${celsius.toFixed(0)}ºF`;
+
+    document
+        .querySelector('[data-js="resultFahrenheit"]').innerHTML = showResultInFahrenheit;
+}
+
+function clearFieldsFahrenheit() {
+    document.querySelector('[data-js="fahrenheit"]').value = "";
+    document
+        .querySelector('[data-js="resultFahrenheit"]').innerHTML = "";
 }
